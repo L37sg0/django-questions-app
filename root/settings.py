36b50e87994503084a06e307db7bb1042decf86e
L37sg0 <petar_1v@abv.bg>
2018-12-12 17:16:39 +0200

    upgrade

--- root/settings.py
+++ root/settings.py
@@ -134,5 +134,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 DISABLE_COLLECTSTATIC=1
